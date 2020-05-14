3729d74c471d05f642f81b17f5741c6db6176515
Jack Quinoa <jquinoa208@gmail.com>
2020-05-14 01:17:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  Elements = ["Wind", "Air", "Water", "Earth"]
+   ["Wind", "Air", "Water", "Earth"]
 end
 
 def add_element_to_end_of_array(array, element)
